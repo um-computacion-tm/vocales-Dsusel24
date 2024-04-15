d92a81ecf1b86fae83b31dff0abce79c1e1a4dee
DSusel24 <tms@MacBook-de-tms.local>
2024-04-15 11:49:09 -0300

    vocales

---
+++ contar_vocales_test.py
@@ -0,0 +1,26 @@
+import unittest
+from contar_vocales import vocal_counter
+from contar_vocales import vocal_counter_des
+
+class TestVocales(unittest.TestCase):
+
+    def test_caso_1(self):
+        esperado = {
+            "a": 5,
+            "e": 1,
+            "u": 1,
+        }
+
+        
+        resultado =vocal_counter("aqui hay una frase")
+        self.assertDictEqual(resultado, esperado)
+
+    def test_caso_2(self):
+        ca, ce, ci, co, cu = vocal_counter_des("ursula")
+        resultado = (ca, ce, ci, co, cu)
+        self.assertEqual(resultado, (1, 0, 0, 0, 2))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
